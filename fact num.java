509142b2122e24ec504eab6d6c3e7a12b0fd1590
keerthanajanani <keerthijanani66@gmail.com>
2017-07-17 10:06:33 +0530

    Create fact num.java

---
+++ fact num.java
@@ -0,0 +1,20 @@
+# fact-num
+import java.io.*;
+import java.util.*;
+public class fact num
+{
+public static void main(String args[])throws IOException
+{
+int n,i,fact=1;
+BufferedReader br=new BufferedReader(new InputStreamReader(br.readLine());
+System.out.println("Enter the input Number:");
+n=Integer.parseInt(br.readLine());
+for(i=1;i<=n;i++)
+{
+fact=fact*i;
+}
+System.out.println("Factorial of "+n+" is : "+fact);
+}
+}
+}
+
